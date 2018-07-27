0bfbe107088eb6260703e96b2f9c8911d524c47b
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-07-27 10:59:42 +0530

    Update vowel.java

--- cse/vowel.java
+++ cse/vowel.java
@@ -12,9 +12,9 @@ class Ideone
 
         char ch ='i';
        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            System.out.println(ch + " is vowel");
+            System.out.println("Vowel");
         else
-            System.out.println(ch + " is consonant");
+            System.out.println("Consonant");
 
 	}
 }
