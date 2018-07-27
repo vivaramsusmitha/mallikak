3998dbd89be46617bbc1221b467f303aa6520071
kommanagamallika <41729787+kommanagamallika@users.noreply.github.com>
2018-07-27 10:51:01 +0530

    Create alphabet.java

---
+++ cse/alphabet.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+       char c = 'a';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(" Alphabet");
+        else
+            System.out.println( "No");
+
+	}
+}
